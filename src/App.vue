b3da4662ac3f6440ea9f388e999c235ec94fa71e
Alaikum <carangiadamo@gmail.com>
2022-09-05 16:10:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HeaderDc/>
+    <MainDc/>
+    <FooterDc/>
+  </div>
+</template>
+
+<script>
+import HeaderDc from './components/HeaderDc.vue';
+import MainDc from './components/MainDc.vue';
+import FooterDc from './components/FooterDc.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    HeaderDc,
+    MainDc,
+    FooterDc
+}
+}
+</script>
+
+<style lang="scss">
+@import './style/index.scss'
+</style>
---
+++ src/components/FooterDc.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>Footer</h3>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
